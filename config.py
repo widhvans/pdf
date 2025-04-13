51763be3bfdac783b6e63b46841f72c9b8c55829
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-13 13:29:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
 # config.py
-BOT_TOKEN = 'YOUR_BOT_TOKEN'  # Replace with your actual bot token from BotFather
+BOT_TOKEN = '7953655254:AAGUxu-olhwtUhdjEigPFnQS8T3zoLNhnXs'  # Replace with your actual bot token from BotFather
