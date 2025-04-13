ffb55be31f0280ebee67b3513d1d43d57aaf7ee5
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-13 13:36:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 # config.py
 BOT_TOKEN = '7953655254:AAGUxu-olhwtUhdjEigPFnQS8T3zoLNhnXs'  # Replace with your actual bot token from BotFather
+WATERMARK_TEXT = 'Study Point Blog'  # Custom watermark text
